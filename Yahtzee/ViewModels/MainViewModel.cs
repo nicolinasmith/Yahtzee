dc7481e9fddf92273c0dc1d6f8788d532ba384e0
Nicolina Smith <113508253+nism07@users.noreply.github.com>
2023-12-06 17:13:32 +0100

    Add project files.

---
+++ Yahtzee/ViewModels/MainViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yahtzee.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private static MainViewModel? _instance;
+
+        public BaseViewModel CurrentViewModel { get; set; } = new StartViewModel();
+
+        public static MainViewModel Instance { get => _instance ?? (_instance = new MainViewModel()); }
+
+        public MainViewModel() { }
+    }
+}
